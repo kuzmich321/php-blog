391b34681ce26ca467fa09f9d328b066592c5e21
enkelvarg <warrrgh666@gmail.com>
2020-09-10 20:27:49 +0300

    just starters

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ define('DEBUG', true);
 define('DEFAULT_CONTROLLER', 'Home'); //default controller of absent in url
 define('DEFAULT_LAYOUT', 'default'); // if no layout is set in da controller
 
-define('GROOT', '/baldr/'); // set dat to '/' in live
+define('GROOT', '/pilot/'); // set dat to '/' in live
 
 define('SITE_TITLE', 'PHP-Blog'); // this is used if no site title is set
 
